a1b42d13929c81da3fbb9eecbcbf752879b8ecd6
Yukari Sakurai <neko3genki@gmail.com>
2018-03-24 21:57:55 +0900

    Implement Help scene

--- PocketCarbo/Controllers/HelpViewController.swift
+++ PocketCarbo/Controllers/HelpViewController.swift
@@ -8,15 +8,47 @@
 
 import UIKit
 
-class HelpViewController: UIViewController {
+class HelpViewController: UIViewController, UIWebViewDelegate {
+
+  @IBOutlet weak var webView: UIWebView!
+  @IBOutlet weak var showTutorialButton: UIButton!
 
   override func viewDidLoad() {
     super.viewDidLoad()
+
+    let htmlPath = Bundle.main.path(forResource: "help", ofType: "html")
+    let url = URL(fileURLWithPath: htmlPath!)
+    let request:URLRequest = URLRequest(url: url)
+    webView.loadRequest(request)
   }
 
   override func viewWillAppear(_ animated: Bool) {
     super.viewWillAppear(animated)
     self.setNavigationBarItem()
   }
-}
 
+  func webView(_ webView: UIWebView, shouldStartLoadWith request: URLRequest, navigationType: UIWebViewNavigationType) -> Bool {
+    switch navigationType {
+    case .linkClicked:
+      // Open links in Safari
+      guard let url = request.url else { return true }
+
+      if #available(iOS 10.0, *) {
+        UIApplication.shared.open(url, options: [:], completionHandler: nil)
+      } else {
+        // openURL(_:) is deprecated in iOS 10+.
+        UIApplication.shared.openURL(url)
+      }
+      return false
+    default:
+      return true
+    }
+  }
+
+  //MARK: Button Action
+
+  @IBAction func didTapTutorial(_ sender: UIButton) {
+    print("TODO : チュートリアル表示")
+  }
+  
+}

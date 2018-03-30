2f5f45b008219993e8861870c706aa067ea46da5
Yukari Sakurai <neko3genki@gmail.com>
2018-03-31 02:34:24 +0900

    Move extension method

--- PocketCarbo/Extensions/UIButton+extention.swift
+++ PocketCarbo/Extensions/UIButton+extention.swift
@@ -38,4 +38,14 @@ import UIKit
       return UIColor(cgColor: color)
     }
   }
+
+  func animateCellButton(completion:@escaping ((Bool) -> Void)) {
+    UIView.animate(withDuration: 0.1, animations: {
+      self.transform = CGAffineTransform(scaleX: 0.6, y: 0.6) }, completion: { (finish: Bool) in
+        UIView.animate(withDuration: 0.1, animations: {
+          self.transform = CGAffineTransform.identity
+          completion(finish)
+        })
+    })
+  }
 }

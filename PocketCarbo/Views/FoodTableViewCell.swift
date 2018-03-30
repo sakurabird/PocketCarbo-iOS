29043ad12ccf7eb132a96f99a3258c15aabcfdf1
Yukari Sakurai <neko3genki@gmail.com>
2018-03-31 02:34:24 +0900

    Move extension method

--- PocketCarbo/Views/FoodTableViewCell.swift
+++ PocketCarbo/Views/FoodTableViewCell.swift
@@ -206,15 +206,3 @@ class FoodTableViewCell: UITableViewCell {
     return str
   }
 }
-
-extension UIButton {
-  func animateCellButton(completion:@escaping ((Bool) -> Void)) {
-    UIView.animate(withDuration: 0.1, animations: {
-      self.transform = CGAffineTransform(scaleX: 0.6, y: 0.6) }, completion: { (finish: Bool) in
-        UIView.animate(withDuration: 0.1, animations: {
-          self.transform = CGAffineTransform.identity
-          completion(finish)
-        })
-    })
-  }
-}

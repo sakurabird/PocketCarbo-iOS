3e078f8d0fcc61589ab778052faa412cc0208c15
Yukari Sakurai <neko3genki@gmail.com>
2018-04-01 02:02:14 +0900

    Implement Setting Scene. But this is unfinished code.

---
+++ PocketCarbo/Extensions/UIAlertController+extension.swift
@@ -0,0 +1,19 @@
+//
+//  UIAlertController+extension.swift
+//  PocketCarbo
+//
+//  Created by Sakura on 2018/03/31.
+//  Copyright © 2018年 Sakura. All rights reserved.
+//
+
+import UIKit
+extension UIAlertController {
+  
+  static func alertWithTitle(title: String, message: String, buttonTitle: String) -> UIAlertController {
+    let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+    let action = UIAlertAction(title: buttonTitle, style: .default, handler: nil)
+    alertController.addAction(action)
+
+    return alertController
+  }
+}
